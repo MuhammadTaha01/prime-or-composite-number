f2bad33537ddd37f73648a3b26a801117204a253
Muhammad Taha <127325759+MuhammadTaha01@users.noreply.github.com>
2023-03-29 00:25:20 +0500

    Add files via upload

---
+++ 6.32.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int c,num,p=1;
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    for(c=2; c<=num/2; c++)
+    if(num%c==0)
+    {
+        p=0;
+        break;
+    }
+    if(p==1)
+    {
+        cout<<num<<" is a prime number";
+    }
+    else
+    {
+        cout<<num<<" is a composite number";
+    }
+    return 0;
+}
